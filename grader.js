597270e391057ab2e6fafe145592202ded0b3105
ldoyle0 <ldoyle0@gmail.com>
2013-07-13 16:44:57 +0000

    added url check to grader.js and loaded more checks into checks.json

--- grader.js
+++ grader.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://pacific-ocean-1627.herokuapp.com";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -12,6 +16,8 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+var checkUrl = function() {
+ 
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -25,6 +31,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
+         
     for (var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
@@ -42,6 +49,7 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('u, --url <url>', 'Path to url', clone(asserFileExists), URL_DEFAULT)
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
